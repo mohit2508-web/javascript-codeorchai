9fc3e4f2e7a55fed3037a7a74a3364d55944266a
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-04-04 22:10:31 +0000

    add datatypes

---
+++ 01_bascis/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all Js code as a newer version
+
+// alert(3+3) // we are using nodejs,not browser
+
+let name="mohit"
+let age=18
+let islogged =false
+let state =null
+
+
+//number 
+// bigint
+//string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol=> unique
+
+
+// object
+
+console.log(typeof "hitesh");
+console.log(typeof age);
+
+console.log(typeof null); // object
+console.log(typeof undefined) // undefined
