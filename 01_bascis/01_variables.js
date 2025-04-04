57fd61fc5675bf1c726ff96049c01348a650d20b
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-04-04 21:57:00 +0000

    know about variable

---
+++ 01_bascis/01_variables.js
@@ -0,0 +1,10 @@
+const accountId=123454
+let accountEmail="mohit@gmail.com"
+var accountPassword="12345";
+accountCity="jaipur"
+
+// accountId =2// not allowed
+
+/* prefer not to use var because of issue in blockspace and functional scope */
+
+console.table([accountEmail,accountId,accountPassword])
