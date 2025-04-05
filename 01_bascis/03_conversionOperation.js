c9e2d7fea2059e0f1f0790457c14e34f45dea02a
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-04-05 06:02:57 +0000

    learn about conversion

---
+++ 01_bascis/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = null
+
+console.log(typeof score);
+
+let string=Number(score);
+
+console.log(typeof string);
+
+/* 
+
+1=> number
+"34" => string
+"34abs" => NaN ->not a number
+true => 1 and false => 0
+undefined => NaN
+null => object
+*/
+
