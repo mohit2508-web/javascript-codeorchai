8b15549a431442f838bf11e73d4f48604b989fc5
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-05-20 17:35:29 +0000

    Strings understanding

---
+++ 01_bascis/04_Strings.js
@@ -0,0 +1,16 @@
+let myname=new String("Mohit Jadon");
+console.log(myname.length);
+
+const lower=myname.toLowerCase();
+console.log(lower);
+
+console.log(myname.replace('M','J'))
+
+console.log(myname.charAt(6))
+
+console.log(myname.toLocaleUpperCase())
+console.log(myname.endsWith('n'))
+
+console.log(myname.split(" "));
+
+console.log(`hello Everyone my name is ${myname} and understand string methods lovingly`)
