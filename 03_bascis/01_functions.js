bf927cd9d2267705b2636b325b6f58f7b8ebd6a3
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-05-22 11:31:27 +0000

    functions ka packup

---
+++ 03_bascis/01_functions.js
@@ -0,0 +1,22 @@
+// function addtwo(num1,num2){
+//     return num1+num2;
+// }
+
+// console.log(addtwo(10,20))
+
+
+// function calculatemycartprice(val1,val2,...num){ // this is known as rest opertor its function is that while you are not aware about no of argument is coming
+//     return num
+
+// }
+// console.log(calculatemycartprice(200,300,432,456,685,357,3,4))
+
+
+const obj={
+    username:"Mohit",
+    age:18
+}
+function user(obj){
+    console.log(`hi username is ${obj.username} and its age is ${obj.age}`)
+}
+user(obj)
