2dfb9f75d84a22bc8fc1fd194f5ac456e298da53
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-05-22 10:33:03 +0000

    object part2

---
+++ 02_bascis/03_objects.js
@@ -0,0 +1,28 @@
+const myobj={
+    objname:"jadugaar",
+    serialno:234,
+    objchild:{
+        childname:"sohit",
+        age:13,
+        sohitchild:{
+            name:"munni",
+            munniage:3
+        }
+
+    }
+}
+
+// console.log(myobj.objchild.sohitchild.name)
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"a",4:"b"}
+const obj3={5:"a",6:"b"}
+
+console.log({obj1,obj2,obj3})
+console.log(Object.assign({},obj1,obj2,obj3))
+console.log({...obj1,...obj2,...obj3})
+
+console.log(Object.keys(myobj))
+console.log(Object.values(myobj))
+console.log(Object.entries(myobj))
+console.log(myobj.hasOwnProperty("objname"))
