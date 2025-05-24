785832551abb754f56110b0c91ed318c1dc91786
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-05-24 09:07:38 +0000

    nco

---
+++ 04_controlflow/one.js
@@ -0,0 +1,27 @@
+// const booked=true
+
+// if(booked){
+//     console.log("chal bhag")
+// }
+
+// Nullish coalescing operator (??) : null undefined
+
+// let val1;
+// val1=5??10
+// val1=null??10
+// val1=undefined??null
+// val1=null??undefined??5
+
+// console.log(val1)
+
+// Ternary operator
+
+// condition?true:false
+
+let price=2000
+price>590?console.log("yes"):console.log("no")
+
+
+// loops 
+// for of loop
+// for in loop => objects // return key
