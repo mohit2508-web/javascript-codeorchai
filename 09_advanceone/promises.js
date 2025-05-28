34104437a96581e997022048d07b56089c57e5f4
Mohit Jadon <mohitjadoonthakur@gmail.com>
2025-05-28 05:24:21 +0000

    fetch

--- 09_advanceone/promises.js
+++ 09_advanceone/promises.js
@@ -30,29 +30,75 @@
 // })
 
 
-const promisefour=new Promise(function(resolve,reject){
+// const promisefour=new Promise(function(resolve,reject){
+//     setTimeout(function(){
+//         let error=false
+//         if(!error){
+//             resolve({username:"mohit jadon",email:"mohit@gmail.com"})
+//         }else{
+//             console.log("Error:Something went wrong")
+//         }
+//     },1000)
+// })
+
+// promisefour.then(function(user){
+//     console.log("promise four resolved")
+//     return user.username
+// }).then(function(user){
+//      console.log(user)
+// }).catch(function(error){
+//     console.log(error)
+// }).finally(function(error){
+//     if(!error){
+//         console.log(`promise is resolved`)
+//     }else{
+//         console.log("promise is rejected")
+//     }
+    
+// })
+
+const promisefive=new Promise(function(resolve,reject){
     setTimeout(function(){
         let error=false
         if(!error){
             resolve({username:"mohit jadon",email:"mohit@gmail.com"})
         }else{
-            console.log("Error:Something went wrong")
+            console.log("Error:Js went wrong")
         }
     },1000)
 })
 
-promisefour.then(function(user){
-    console.log("promise four resolved")
-    return user.username
-}).then(function(user){
-     console.log(user)
-}).catch(function(error){
-    console.log(error)
-}).finally(function(error){
-    if(!error){
-        console.log(`promise is resolved`)
-    }else{
-        console.log("promise is rejected")
+
+async function consumepromisefive(params) {
+    try{
+   const respnose= await promisefive
+   console.log(respnose)
+    }catch(error){
+        console.log(error)
     }
+}
+// consumepromisefive();
+
+
+// async function getAllUsers(){
+//     try{
     
+//         const response=await fetch('https://api.github.com/users/hiteshchoudhary')
+//         const data=await response.json()
+//         console.log(data)
+//     }catch(error){
+//          console.log("E: ",error)
+//     }
+// }
+// getAllUsers()
+
+fetch('https://api.github.com/users/hiteshchoudhary')
+.then(function(response){
+    return response.json()
+})
+.then(function(data){
+    console.log(data)
+})
+.catch(function(error){
+    console.log(error)
 })
